51444863b103014abfa05fa1fb621db56d992472
Flavio Romero <flavio.arc9@gmail.com>
2022-06-30 01:23:33 -0500

    New Order

--- docs/.vuepress/config.ts
+++ docs/.vuepress/config.ts
@@ -6,7 +6,7 @@ export default defineUserConfig({
   lang: 'es-ES',
   // title : 'Tsoft',
   description: 'Documentacion de cypress framework e2e Tsoft',
-  port: 8081,
+  // port: 8081,
   open: false,
   head: [['link', { rel: 'icon', href: '/images/logo.png' }]],
   theme: defaultTheme({
